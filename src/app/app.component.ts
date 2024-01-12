d64c53f3d8146a9560d5ced2a2bd35ae282f523f
Nagaraj <nagaraj65363@gmail.com>
2024-01-12 18:35:57 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'YT-Playlist-Angular';
+  title = 'YT Playlist Player Angular';
+  PlaylistID:any="PLHTZwvmFtBB-4Fqkid3Zx242eFm0V-tSw";
+  
+  dangerousVideoUrl!: string;
+  IframeShow:boolean= false;
+  videoUrl!: SafeResourceUrl;
+  applyForm = new FormGroup({
+    youtubePlaylistId: new FormControl(''),
+    
+  });
+  constructor(private sanitizer: DomSanitizer) {
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/videoseries?list='+this.PlaylistID+'&controls=0&rel=0&showinfo=0&autoplay=1';
+  
+  }
+  
+
+  updateVideoUrl() {
+   
+    this.IframeShow = true;
+    this.PlaylistID = this.applyForm.value.youtubePlaylistId ?? "";
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/videoseries?list='+this.PlaylistID+'&controls=0&rel=0&showinfo=0&autoplay=1';
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+    this.applyForm.reset();
+  }
+
+
 }
